9a2f48484af8fac15bfbdf6b3b697ba511795c65
Paulo Phagula <pphagula@gmail.com>
2016-02-28 20:37:33 +0200

    4 space alingment

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,51 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	plumber = require('gulp-plumber'),
-  jshint = require('gulp-jshint'),
-  sh = require('shelljs'),
-  shell = require('gulp-shell'),
-  concat = require('gulp-concat'),
-  uglify  = require('gulp-uglify')
+  	gutil = require('gulp-util'),
+  	plumber = require('gulp-plumber'),
+    jshint = require('gulp-jshint'),
+    sh = require('shelljs'),
+    shell = require('gulp-shell'),
+    concat = require('gulp-concat'),
+    uglify  = require('gulp-uglify')
 ;
 
 gulp.task('default', ['lint', 'build']);
 
 var onError = function(error){
-	gutil.beep();
-	console.log(error);
-}
+  	gutil.beep();
+  	console.log(error);
+};
 
 gulp.task('build', function(){
-  gulp
-    .src([
-      'src/f.js', 
-      'src/ext.util.js',
-      'src/core.js',
-      'src/dispatcher.js',
-      'src/sandbox.js',
-      'src/store.js',
-      'src/module.js',
-      'src/router.js',
-      'src/ext.http.js',
-      'src/ext.dom.js',
-      'src/ext.storage.js',
+    gulp.src([
+        'src/f.js', 
+        'src/ext.util.js',
+        'src/core.js',
+        'src/dispatcher.js',
+        'src/sandbox.js',
+        'src/store.js',
+        'src/module.js',
+        'src/router.js',
+        'src/ext.http.js',
+        'src/ext.dom.js',
+        'src/ext.storage.js',
     ])
     .pipe(concat('f.js'))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build-min', function(){
-  gulp
-    .src([
-      'src/f.js', 
-      'src/ext.util.js',
-      'src/core.js',
-      'src/dispatcher.js',
-      'src/sandbox.js',
-      'src/store.js',
-      'src/module.js',
-      'src/router.js',
-      'src/ext.http.js',
-      'src/ext.dom.js',
-      'src/ext.storage.js',
+    gulp.src([
+        'src/f.js', 
+        'src/ext.util.js',
+        'src/core.js',
+        'src/dispatcher.js',
+        'src/sandbox.js',
+        'src/store.js',
+        'src/module.js',
+        'src/router.js',
+        'src/ext.http.js',
+        'src/ext.dom.js',
+        'src/ext.storage.js',
     ])
     .pipe(uglify({preserveComments: 'some'}))
     .pipe(concat('f.min.js'))
@@ -59,7 +57,7 @@ gulp.task('watch', function(){
 });
 
 gulp.task('lint', function(){
-  return gulp.src('./src/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    return gulp.src('./src/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
