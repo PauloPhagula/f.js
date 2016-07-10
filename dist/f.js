c85ee8fa90942cf0e9bfa1b3a1a7becaabae82cd
Paulo Phagula <pphagula@gmail.com>
2016-07-10 13:43:47 +0200

    bump SemVer for new release

--- dist/f.js
+++ dist/f.js
@@ -1,5 +1,5 @@
 /*!
- * f, v0.1.4 (2016/07/10 11:32)
+ * f, v0.1.5 (2016/07/10 11:43)
  * A JavaScript framework for modular and scalable SPAs.
  * <https://github.com/dareenzo/f-es>
  *
@@ -21,7 +21,7 @@ var factory = function (Navigo) {
     var previousF = F;
 
     // Current version of the library. Keep in sync with `package.json` and `bower.json`.
-    F.VERSION = '0.1.4';
+    F.VERSION = '0.1.5';
 
     // Set framework to debug mode. Disabled by default
     F.DEBUG = false;
