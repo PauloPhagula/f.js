930db65e691a459f33c914325cd8e84a890ea997
Paulo Phagula <pphagula@gmail.com>
2017-03-25 08:54:35 +0200

    Polyfill Object.assign since we targe ES5

--- src/f.js
+++ src/f.js
@@ -49,11 +49,37 @@
         return descriptors;
     };
 
+    // Polyfill Object.assign
+    // taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+    if (typeof Object.assign != 'function') {
+        Object.assign = function(target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target == null) { // TypeError if undefined or null
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+
+                if (nextSource != null) { // Skip over if undefined or null
+                    for (var nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        };
+    }
+
     // Util
     // ---
 
     // Composes objects by combining them into a new one
-    // TODO: Object.assign only exists in ES6 and library is for ES5 so fix this.
     F.compose = Object.assign;
 
     /**
